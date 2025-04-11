2b3b9dca34f173eba34ae56090d68d4115b357e6
Ked <balkangrid@gmail.com>
2025-04-11 11:08:38 +0800

    Changed the lsp server setups to be cleaner

--- lua/lsp_server_setups.lua
+++ lua/lsp_server_setups.lua
@@ -1,51 +1,44 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
-
+require("neodev").setup()
 local installed_servers = require("mason-lspconfig").get_installed_servers()
-
-require("neodev").setup({})
-
 local lspconfig = require("lspconfig")
-
-local lua_ls_setup = {
-    settings = {
-        Lua = {
-            completion = {
-                callSnippet = "Replace"
-            },
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { "vim" }
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-        }
-    },
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
-}
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Setup each server dynamically
 for _, server in ipairs(installed_servers) do
     if server == "lua_ls" then
         -- Custom setup for Lua
-        lspconfig.lua_ls.setup(lua_ls_setup)
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    completion = {
+                        callSnippet = "Replace"
+                    },
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { "vim" }
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                }
+            },
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
     elseif server == "clangd" then
-        lspconfig.clangd.setup {
-            capabilities = capabilities,
+        lspconfig.clangd.setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             cmd = { "clangd", "--fallback-style=webkit" }
-        }
+        })
     elseif server == "hls" then
-        lspconfig.hls.setup {
-            capabilities = capabilities,
+        lspconfig.hls.setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             settings = {
                 haskell = {
                     plugin = {
@@ -57,9 +50,9 @@ for _, server in ipairs(installed_servers) do
                     }
                 }
             }
-        }
+        })
     else
         -- Default setup for all other servers
-        lspconfig[server].setup { capabilities = capabilities }
+        lspconfig[server].setup { capabilities = require("cmp_nvim_lsp").default_capabilities() }
     end
 end
